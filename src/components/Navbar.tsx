d744f7c2f42a17fbd3652a00d4a06ad17c3cc4f5
farah2003 <farah.shaqoura@example.com>
2023-05-23 14:08:04 +0300

    eddit filter section4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo2.png';
 const Navbar = () => {
     return (
         <Flex direction="row" align="center" px={7}>
-            <Image src={logo} boxSize="40px" mb={2} mr={2} />
+            <Image src={logo} boxSize="40px" mb={2} mr={3} />
             <Text fontWeight="bold" fontSize="xl">World Finder</Text>
         </Flex>
     );
